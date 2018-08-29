9f89c5874236a45a615772249e26aab463926079
pprogs <kchurilov@navicons.ru>
2018-08-29 18:26:59 +0300

    deploy

---
+++ precache-manifest.271ed8a733ba36540842595cbd88c60a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/assets/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "368b66dccd91181ab1bdaaab76f1eaf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "133c4d51aa21faadda3e",
+    "url": "/assets/js/chunk-vendors.133c4d51.js"
+  },
+  {
+    "revision": "2092cf253a9fe4745029",
+    "url": "/assets/js/app.2092cf25.js"
+  },
+  {
+    "revision": "842bda405ce1ae40b63e",
+    "url": "/assets/js/about.842bda40.js"
+  },
+  {
+    "revision": "e046506e41791980a886b225b6212ccd",
+    "url": "/assets/img/zap.e046506e.jpg"
+  },
+  {
+    "revision": "f41f1c691fa07b125cb34f6dd7019ebb",
+    "url": "/assets/img/rest.f41f1c69.jpg"
+  },
+  {
+    "revision": "1acb56fd19253bb052d51ed357a9cf03",
+    "url": "/assets/img/palatka.1acb56fd.jpg"
+  },
+  {
+    "revision": "14aedc61d4f624ee3570d52bc3501000",
+    "url": "/assets/img/magaz.14aedc61.jpg"
+  },
+  {
+    "revision": "133c4d51aa21faadda3e",
+    "url": "/assets/css/chunk-vendors.3bfc1a7a.css"
+  },
+  {
+    "revision": "2092cf253a9fe4745029",
+    "url": "/assets/css/app.c9ca3d1f.css"
+  }
+];
